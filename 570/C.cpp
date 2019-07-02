44445e8629ed1e59a974e1823eefc50a3e85c030
Tiago Lisboa <tiago.caio.ol@gmail.com>
2019-07-02 15:31:11 -0300

    570 C finished

--- 570/C.cpp
+++ 570/C.cpp
@@ -10,7 +10,7 @@ int main () {
 	while (q--) {
 		scanf("%d %d %d %d", &k, &n, &a, &b);
 
-		printf("%d %d %d %d\n", k, n, a, b);
+		//printf("%d %d %d %d\n", k, n, a, b);
 		if (k > a*n) {
 			printf("%d\n", n);
 			continue;
@@ -21,19 +21,19 @@ int main () {
 			continue;
 		}
 
-		int l = 0, r = n-1, m;
-		printf ("asdfasdf %d %d %d n: %d\n", l, r, m, n);
+		int l = 0, r = n+1, m;
+		//printf ("asdfasdf %d %d %d n: %d\n", l, r, m, n);
 		int i = 0;
 		while (l < r) {
-			m = (r - l)/2;
-			if (k > (m*b)+((n-m)*a)) {
+			m = (r + l)/2;
+			if (k > (m*a)+((n-m)*b)) {
 				l = m+1;
 			} else {
 				r = m-1;
 			}
 		}
 
-		printf("%d\n", m);
+		printf("%d\n", l);
 	}
 
 	return 0;
